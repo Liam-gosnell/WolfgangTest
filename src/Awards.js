74f987eb6857bf44d4d9f1d2bc21371bc7886a00
Liam-gosnell <liamgoz99@gmail.com>
2021-11-16 20:58:44 +0000

    Added columns and rows to filter section

--- src/Awards.js
+++ src/Awards.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Awards.css'
 
 function Awards() {
     return (
-        <div className="awards container">
-            <h1>AWARDS SECTION</h1>
+        <div className="awards">
+            <div className="container">
+                <div className="row">
+                    <div className="column col-filters">
+                        <p>FILTER</p>
+                    </div>
+                    <div className="column col-awards">
+                        <p>Viewing 14 of 75 awards</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
