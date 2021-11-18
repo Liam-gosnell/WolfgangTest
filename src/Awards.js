6855ba85dd3e528b8833ca19268466bd21c7b9c4
Liam-gosnell <liamgoz99@gmail.com>
2021-11-18 19:00:49 +0000

    Added awards grouping on right

--- src/Awards.js
+++ src/Awards.js
@@ -19,6 +19,45 @@ function Awards() {
                             </span>
                         </button>
                     </div>
+                    <div className="category">
+                        <button className="button-toggle" onClick="toggleCategoryList('#years')">
+                            <span>By Year</span>
+                            <span id="years-icon" className="--closed">
+                                <KeyboardArrowDownIcon />
+                            </span>
+                        </button>
+                    </div>
+                    <div className="category">
+                        <button className="button-toggle" onClick="toggleCategoryList('#channels')">
+                            <span>By Channel</span>
+                            <span id="channels-icon" className="--closed">
+                                <KeyboardArrowDownIcon />
+                            </span>
+                        </button>
+                    </div>
+                    <div className="category">
+                        <button className="button-toggle" onClick="toggleCategoryList('#industries')">
+                            <span>By Industry</span>
+                            <span id="industries-icon" className="--closed">
+                                <KeyboardArrowDownIcon />
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div className="awards">
+                    <p> Viewing <span id="award-count">13</span> of 75 awards </p>
+                    <div className="awards-group">
+                        <h2 className="year">
+                            2021 
+                        <span>Click on the award to read more about the winning entry</span>
+                        <span><KeyboardArrowDownIcon /></span>
+                        </h2>
+                        <div className="container-2021">
+                            {/* <div className="ceremony">
+                                
+                            </div> */}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
