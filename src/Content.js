0f386c5e7fb9d9af1e48bf5ddbc54039bc52ec2b
Liam-gosnell <liamgoz99@gmail.com>
2021-12-09 15:04:51 +0000

    Changed spacing

--- src/Content.js
+++ src/Content.js
@@ -7,9 +7,11 @@ function Content() {
         <div className="content container">
             <div class='row'>
                 <div class='column double-column'>
+
                      <div class='text-container main-paragraph'>
                         <p>Back in 2011, when Wolfgang was only two people we submitted some case studies to the Digital Media Awards, optimistically entering the "Best Agency" category. At that time nobody knew who we were and our clients were not of the "catwalk" variety. So we certainly didn't expect to win, we optimistically hoped to get one of those cool "finalist" badges for our email signatures. Lo and behold, the judges loved our case studies, scored them strongly and we took the Gold Medal* home. </p>
                         <p>This success imbued us with a wolfish hunger to execute outstanding campaigns and win further domestic & international awards.</p>
+                        
                     </div>
                  </div>
                 <div class='column'>
